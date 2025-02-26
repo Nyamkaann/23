335022f811e4bfc282beb4b1ea99dcaab9fa4e19
Nyamdorj <nyamdorjmunkh@gmail.com>
2025-02-26 17:47:07 +0800

    123

--- src/data/initialFunding.ts
+++ src/data/initialFunding.ts
@@ -1,17 +1,17 @@
 export const initialFunding = [
-  { id: "1", name: "*****", amount: 100000 }, //Nyamdorj
-  { id: "2", name: "*****", amount: 100000 }, //Erdka
-  { id: "3", name: "*****", amount: 50000 }, //Tempo
-  { id: "4", name: "*****", amount: 100000 }, //Namuunzul
-  { id: "5", name: "*****", amount: 50000 }, //Tengis      
-  { id: "6", name: "*****", amount: 50000 }, //Dalii
-  { id: "7", name: "*****", amount: 50000 }, //Marlaa
-  { id: "8", name: "*****", amount: 95000 }, //Ariundari
-  { id: "9", name: "*****", amount: 95000 }, //Bayraa
-  { id: "10", name: "*****", amount: 95000 }, //Bolor
-  { id: "11", name: "*****", amount: 95000 }, //Dodkoo
-  { id: "12", name: "*****", amount: 95000 }, //Tsolmon
-  { id: "13", name: "*****", amount: 95000 }, //Narangoo
-  { id: "14", name: "*****", amount: 60000 }, //Nomio
-  { id: "15", name: "*****", amount: 30000 }, //Mino
+  { id: "1", name: "N*****", amount: 100000 }, //Nyamdorj
+  { id: "2", name: "E*****", amount: 100000 }, //Erdka
+  { id: "3", name: "T*****", amount: 50000 }, //Tempo
+  { id: "4", name: "N*****", amount: 100000 }, //Namuunzul
+  { id: "5", name: "T*****", amount: 50000 }, //Tengis      
+  { id: "6", name: "D*****", amount: 50000 }, //Dalii
+  { id: "7", name: "M*****", amount: 50000 }, //Marlaa
+  { id: "8", name: "A*****", amount: 95000 }, //Ariundari
+  { id: "9", name: "B*****", amount: 95000 }, //Bayraa
+  { id: "10", name: "B*****", amount: 95000 }, //Bolor
+  { id: "11", name: "D*****", amount: 95000 }, //Dodkoo
+  { id: "12", name: "T*****", amount: 95000 }, //Tsolmon
+  { id: "13", name: "N*****", amount: 95000 }, //Narangoo
+  { id: "14", name: "N*****", amount: 60000 }, //Nomio
+  { id: "15", name: "M*****", amount: 30000 }, //Mino
 ]; 
