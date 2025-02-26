859d386a4bc876bc92b3f363f34d91ccbc0838d4
Nyamdorj <nyamdorjmunkh@gmail.com>
2025-02-26 21:13:30 +0800

    name mask off

--- src/data/initialFunding.ts
+++ src/data/initialFunding.ts
@@ -1,20 +1,20 @@
 export const initialFunding = [
-  { id: "1", name: "******", amount: 100000 }, //Nyamdorj
-  { id: "2", name: "******", amount: 100000 }, //Erdka
+  { id: "1", name: "Nyamdorj", amount: 100000 }, //Nyamdorj
+  { id: "2", name: "Erdka", amount: 100000 }, //Erdka
   { id: "3", name: "TEMPO", amount: 50000, profilePic: "/tempo.jpg" }, //Tempo
-  { id: "4", name: "******", amount: 100000 }, //Namuunzul
-  { id: "5", name: "******", amount: 50000 }, //Tengis      
-  { id: "6", name: "******", amount: 50000 }, //Dalii
-  { id: "7", name: "******", amount: 50000 }, //Marlaa
-  { id: "8", name: "******", amount: 95000 }, //Ariundari
-  { id: "9", name: "******", amount: 95000 }, //Bayraa
-  { id: "10", name: "******", amount: 95000 }, //Bolor
-  { id: "11", name: "******", amount: 95000 }, //Dodkoo
-  { id: "12", name: "******", amount: 95000 }, //Tsolmon
-  { id: "13", name: "******", amount: 95000 }, //Narangoo
-  { id: "14", name: "******", amount: 60000 }, //Nomio
-  { id: "15", name: "******", amount: 30000 }, //Mino
-  { id: "16", name: "******", amount: 70000 }, //Ba Khulan
-  { id: "17", name: "******", amount: 15000 }, //Khorvoo
-  { id: "18", name: "******", amount: 100000 }, //Anar
+  { id: "4", name: "Zul", amount: 100000 }, //Namuunzul
+  { id: "5", name: "Tengis", amount: 50000 }, //Tengis      
+  { id: "6", name: "Dalii", amount: 50000 }, //Dalii
+  { id: "7", name: "Maralaa", amount: 50000 }, //Marlaa
+  { id: "8", name: "Ariundari", amount: 95000 }, //Ariundari
+  { id: "9", name: "Bayraa", amount: 95000 }, //Bayraa
+  { id: "10", name: "Bolor", amount: 95000 }, //Bolor
+  { id: "11", name: "Dodkoo", amount: 95000 }, //Dodkoo
+  { id: "12", name: "Tsolmon", amount: 95000 }, //Tsolmon
+  { id: "13", name: "Narangoo", amount: 95000 }, //Narangoo
+  { id: "14", name: "Nomio", amount: 60000 }, //Nomio
+  { id: "15", name: "Mino", amount: 30000 }, //Mino
+  { id: "16", name: "Ba Khulan", amount: 70000 }, //Ba Khulan
+  { id: "17", name: "Khorvoo", amount: 15000 }, //Khorvoo
+  { id: "18", name: "Anar", amount: 100000 }, //Anar
 ]; 
